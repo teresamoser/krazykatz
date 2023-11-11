2f7d71d5f524d87b8f3c54dd2ef4d1d765bf9f9c
hugolalmeida <hugogule@gmail.com>
2023-11-11 15:53:49 -0300

    Hugo - Nodejs setup, Readme and Swagger.js

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Krazy Katz Project API',
+    description: 'Lesson 9-13 Team Project - Krazy Katz API'
+  },
+  host: 'localhost:8080',
+  schemes: ['http', 'https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
