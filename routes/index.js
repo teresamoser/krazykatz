a0ccff62d09e7e56500d0a89595a5edcd856b27e
Teresa Moser <moserte@byui.edu>
2023-11-18 20:25:54 -0700

    TM-update to index file for veterinarians

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,14 @@ const swagger = require('./swagger');
 const appointments = require('./appointments');
 const pets = require('./pets');
 // const users = require('./users');
-// const veterinarians = require('./veterinarians');
+const veterinarians = require('./veterinarians');
 // const auth0 = require('./auth0');
 
 router.use('/api-docs', swagger);
 router.use('/appointments', appointments);
 router.use('/pets', pets);
 // router.use('/users', users);
-// router.use('/veterinarians', veterinarians);
+router.use('/veterinarians', veterinarians);
 router.use('/', homeController.home);
 
 // DO NOT UNCOMMENT until Auth0 is working -Nina
