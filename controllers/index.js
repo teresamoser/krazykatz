2a65913555f1d74cd469b5ff5bc2f2577dcd437a
lemosjuliana <lemos.julianajesus@gmail.com>
2023-11-22 21:25:26 -0300

    JL - trying to fix css

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const home = async (req, res) => {
   try {
-    res.sendFile(path.join(__dirname, 'frontend/index.html'), { headers: { 'Content-Type': 'text/html' } });
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
