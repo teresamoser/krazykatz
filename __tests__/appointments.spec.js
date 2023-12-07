59602b7c7220187b6d2d21614720e4e18b08c38f
lemosjuliana <lemos.julianajesus@gmail.com>
2023-12-06 22:09:53 -0300

    JL - appointments - test

---
+++ __tests__/appointments.spec.js
@@ -0,0 +1,41 @@
+// Test for getAllAppointments function
+
+const appointmentsController = require('../controllers/appointments');
+const mongodb = require('../db/connect');
+
+describe('getAllAppointments', () => {
+  it('should return all appointments', async () => {
+    const mockAppointments = [{ /* mocked appointment data */ }];
+    jest.spyOn(mongodb.getDb().db().collection('appointments'), 'find').mockReturnValue({
+      toArray: jest.fn().mockResolvedValue(mockAppointments),
+    });
+
+    const req = {};
+    const res = {
+      setHeader: jest.fn(),
+      status: jest.fn(),
+      json: jest.fn(),
+    };
+
+    await appointmentsController.getAllAppointments(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockAppointments);
+  });
+
+  it('should handle errors and return 500 status', async () => {
+    jest.spyOn(mongodb.getDb().db().collection('appointments'), 'find').mockRejectedValue(new Error('Mocked error'));
+
+    const req = {};
+    const res = {
+      setHeader: jest.fn(),
+      status: jest.fn(),
+      json: jest.fn(),
+    };
+
+    await appointmentsController.getAllAppointments(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' });
+  });
+});
